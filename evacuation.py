67dc4f362499cc000b900982bfdc69efd177c36b
unknown <matteo.butano@gmail.com>
2023-04-06 14:31:45 +0200

    It is now possible to choose between an agent-based simulation and a Mean-Field Game

--- evacuation.py
+++ evacuation.py
@@ -5,8 +5,8 @@ import json
 
 # Create class to describe simulation
 
-class simulation():
-    def __init__(self,optimal = False,T = 10,densities = []):
+class simulation:
+    def __init__(self, mode,T = 10):
         with open('abm_evacuation/config.json') as f:
             var = json.loads(f.read())
         
@@ -18,11 +18,72 @@ class simulation():
         # Read room 
         self.room_length = var['room_length']
         self.room_height = var['room_height']
+        self.pot = var['hjb_params']['potential']
         
-        # Read params for density plot
         self.Nx = var['Nx']
         self.Ny = var['Ny']
         self.sigma_convolution = var['sigma_convolution']
+        
+             
+        self.dx = self.room_length/(self.Nx-1)
+        self.dy = self.room_height/(self.Ny-1)
+
+        self.X_opt, self.Y_opt = np.meshgrid(np.linspace(0,self.room_length,self.Nx)
+                                             ,np.linspace(0,self.room_height,self.Ny))
+        
+        self.u_0 = np.zeros((self.Ny,self.Nx),dtype = float)
+        self.phi_0 =  np.zeros((self.Ny,self.Nx),dtype = float)
+        self.evacuator = np.zeros((self.Ny,self.Nx),dtype = float) + 1
+        
+        self.V = np.zeros((self.Ny,self.Nx)) + self.pot
+        self.V[1:-1,1:-1] = 0
+        self.lim = 10e-6
+        
+        for walls in var['walls']:
+            wall = var['walls'][walls]
+
+            mask_X = abs(self.X_opt-wall[0]) < wall[2]/2
+            mask_Y = abs(self.Y_opt-wall[1]) < wall[3]/2
+           
+            
+            V_temp = np.zeros((self.Ny,self.Nx))
+            
+            V_temp[mask_X*mask_Y] = self.pot
+            
+            self.V += V_temp
+         
+        for holes in var['holes']:
+            hole = var['holes'][holes]
+        
+            hole_X = abs(self.X_opt-hole[0]) < hole[2]/2
+            hole_Y = abs(self.Y_opt-hole[1]) < hole[3]/2
+            
+            self.V[hole_X*hole_Y] = 0   
+        
+        for cyls in var['cylinders']:
+            cyl = var['cylinders'][cyls]
+            
+            V_temp =  np.zeros((self.Ny,self.Nx))
+            
+            V_temp[np.sqrt((self.X_opt-cyl[0])**2 + (self.Y_opt-cyl[1])**2) < cyl[2]] = self.pot
+            
+            self.V+= V_temp
+        
+        self.V = self.pot *(self.V <= self.pot)  
+        
+        for door in var['doors']:
+            door = var['doors'][door]
+            if door[2] > 0:
+                door_X = abs(self.X_opt - door[0]) < door[2]/2
+                door_Y = abs(self.Y_opt - door[1]) < 0.2*door[2]
+            if door[3] > 0:
+                door_Y = abs(self.Y_opt - door[1]) < door[3]/2
+                door_X = abs(self.X_opt - door[0]) < 0.2*door[3]
+            self.evacuator[door_X*door_Y] = 0
+            self.V[door_X*door_Y] = door[4]
+            self.phi_0[door_X*door_Y] = door[4]
+            
+        self.phi_0 = self.phi_0.reshape(self.Nx*self.Ny)
         
         # Init time variables 
         self.time = 0.
@@ -39,60 +100,80 @@ class simulation():
         self.rep_int = var['repulsion_intensity']
         self.noise_intensity = var['noise_intensity']
         self.des_v = var['des_v']
-        
-        # Create crowd
-        self.agents = np.empty(self.N,dtype=object)
         self.initial_box = var['initial_box']
-        xs = np.random.uniform(self.initial_box[0], self.initial_box[1],self.N)
-        ys = np.random.uniform(self.initial_box[2],self.initial_box[3],self.N)
-        for i in range(self.N):
-            self.agents[i] = ped(xs[i], ys[i], 0, 0, self.doors, self.room_length, self.room_height)
-            self.agents[i].choose_target()
-        print('Simulation room created!')
-        
-        # Create list to store the density at each instant
-        self.densities = []
-        self.densities.append(self.gaussian_density(self.sigma_convolution, self.Nx, self.Ny)[2])
-        
-        # Load path optimization 
-        self.is_optimal = optimal
-        
-        if self.is_optimal :
-            self.optimal = optimal_trajectories(T=T,densities = densities)
-            self.optimal.compute_optimal_velocity()
+        self.agents = np.empty(self.N,dtype=object)
+        self.initial_density = var['hjb_params']['initial_density']
+        self.m_0 = np.zeros((self.Ny,self.Nx),dtype = float) 
             
+        self.type = mode
+        
+        self.optimal = optimal_trajectories(T)
+        
+        if self.type == 'abm':
+           # Create crowd
+           xs = np.random.uniform(self.initial_box[0], self.initial_box[1],self.N)
+           ys = np.random.uniform(self.initial_box[2],self.initial_box[3],self.N)
+           for i in range(self.N):
+               self.agents[i] = ped(xs[i], ys[i], 0, 0, self.doors, self.room_length, self.room_height)
+               self.agents[i].choose_target()
+               
+           print('ABM simulation room created!')
+           
+           
+           self.optimal.compute_optimal_velocity()
+        
+        elif self.type =='mfg':
+           x_min = self.initial_box[0]
+           x_max = self.initial_box[1]
+           y_min = self.initial_box[2]
+           y_max = self.initial_box[3]
+           X = self.X_opt
+           Y = self.Y_opt
+           self.m_0[((X > x_min) & (X < x_max)) * ((Y > y_min) & (Y < y_max))] = self.initial_density 
+        
+           print('MFG simulation room created!')
+        
+   
         
     def draw(self,mode = 'scatter'):
-        if mode == 'scatter':
-            scat_x = [self.agents[i].position()[0] for i in range(self.N) if self.agents[i].status]
-            scat_y = [self.agents[i].position()[1] for i in range(self.N) if self.agents[i].status]
-            plt.scatter(scat_x,scat_y,color = 'blue')
         
-        if mode == 'arrows':
-            if self.inside > 0:
+        if self.type == 'abm':
+        
+            if mode == 'scatter':
                 scat_x = [self.agents[i].position()[0] for i in range(self.N) if self.agents[i].status]
                 scat_y = [self.agents[i].position()[1] for i in range(self.N) if self.agents[i].status]
-                scat_vx = [self.agents[i].velocity()[0] for i in range(self.N) if self.agents[i].status]
-                scat_vy = [self.agents[i].velocity()[1] for i in range(self.N) if self.agents[i].status]
-                plt.quiver(scat_x,scat_y,scat_vx, scat_vy,color = 'blue')
-            else:
-                plt.plot()                 
-        
-        if mode == 'density':
-            X,Y,d = self.gaussian_density(self.sigma_convolution, self.Nx, self.Ny)
-            plt.pcolor(X,Y,d.reshape(self.Ny,self.Nx))
-            plt.colorbar()
+                plt.scatter(scat_x,scat_y,color = 'blue')
+             
+            if mode == 'arrows':
+                if self.inside > 0:
+                    scat_x = [self.agents[i].position()[0] for i in range(self.N) if self.agents[i].status]
+                    scat_y = [self.agents[i].position()[1] for i in range(self.N) if self.agents[i].status]
+                    scat_vx = [self.agents[i].velocity()[0] for i in range(self.N) if self.agents[i].status]
+                    scat_vy = [self.agents[i].velocity()[1] for i in range(self.N) if self.agents[i].status]
+                    plt.quiver(scat_x,scat_y,scat_vx, scat_vy,color = 'blue')
+                else:
+                    plt.plot()                 
+            
+            if mode == 'density':
+                X,Y,d = self.gaussian_density(self.sigma_convolution, self.Nx, self.Ny)
+                plt.pcolor(X,Y,d.reshape(self.Ny,self.Nx))
+                plt.colorbar()
+            
+        elif self.type == 'mfg':
+            
+            plt.pcolor(self.X_opt, self.Y_opt,self.m_0 + 2*self.initial_density*self.V/self.pot)
+            
             
         # for door in self.doors:
         #     plt.plot([door[0]-door[2]/2, door[0] + door[2]/2],[door[1] - door[3]/2, door[1] + door[3]/2], 'r-', linewidth=4)
         
-        plt.imshow(np.flip(self.optimal.V,axis = 0),extent=[0,self.room_length,0,self.room_height])
+        plt.imshow(np.flip(self.V,axis = 0),extent=[0,self.room_length,0,self.room_height])
         plt.xlim([0,self.room_length])
         plt.ylim([0,self.room_height])
         title = 't = {:.2f}s exit = {}/{}'.format(self.time,self.N - self.inside,self.N)
         plt.title(title)
+        plt.show()
             
-    
     def step(self,dt,verbose =  False):
         for i in  np.random.choice(np.arange(self.N),self.N,replace=False):
             # Summon agent
@@ -102,7 +183,7 @@ class simulation():
             if agent.status:
             
                 # Compute desired velocity 
-                if self.optimal and self.simu_step < self.optimal.nt_opt:
+                if self.simu_step < self.optimal.nt_opt:
                     vx_opt,vy_opt = self.optimal.choose_optimal_velocity(agent.position(), self.simu_step)
                     des_x, des_y = self.des_v*np.array((vx_opt,vy_opt),dtype = float) 
                 
@@ -178,16 +259,23 @@ class simulation():
             ys[i] = agent.initial_position[1]
         return xs,ys
 
-    def run(self, verbose = False, draw = False,mode = 'scatter'):
+    def run(self, verbose = True, draw = False, mode = 'scatter'):
         
-        while self.inside > 0:    
-            self.step(self.dt,verbose = verbose)
-            self.densities.append(self.gaussian_density(self.sigma_convolution, self.Nx, self.Ny)[2])
-            if draw:
-                self.draw(mode)
-                plt.show()
+        if self.type == 'abm':
+        
+            while self.inside > 0:    
+                self.step(self.dt,verbose = verbose)
+                if draw:
+                    self.draw(mode)
+                    plt.show()
+                
+            print('Evacuation complete!')
             
-        print('Evacuation complete!')
+        elif self.type == 'mfg':
+            
+            self.optimal.mean_field_game(self.m_0,draw = draw,verbose=verbose)
+            
+            
                 
     def gaussian_density(self,sigma,Nx,Ny):
         
@@ -307,7 +395,7 @@ class ped:
         
 
 class optimal_trajectories:
-    def __init__(self,T = 5,densities = []):
+    def __init__(self,T):
         
         with open('abm_evacuation/config.json') as f:
             var = json.loads(f.read())
@@ -319,7 +407,7 @@ class optimal_trajectories:
         for i,door in enumerate(var['doors']):
             self.doors[i,:] = np.array(var['doors'][str(door)])
         
-        # Read HJB parameters
+        # Read optimization parameters
         self.Nx = var['Nx']
         self.Ny = var['Ny']
         self.g = var['hjb_params']['g']
@@ -327,19 +415,10 @@ class optimal_trajectories:
         self.mu = var['hjb_params']['mu']
         self.pot = var['hjb_params']['potential']
         
-        # Init density 
-        self.densities = np.array(densities)
-            
-        # Init time variables 
+        
         self.dt = var['dt']
-        
-        if len(densities) > 0:
-            self.nt_opt = self.densities.shape[0]
-            self.T = self.dt*self.nt_opt
-        
-        else: 
-            self.T = T
-            self.nt_opt = round(self.T/self.dt)
+        self.T = T
+        self.nt_opt = round(self.T/self.dt)
             
         # Create boundary conditions, doors and final cost
         
@@ -356,12 +435,9 @@ class optimal_trajectories:
         self.phi_0 =  np.zeros((self.Ny,self.Nx),dtype = float)
         self.evacuator = np.zeros((self.Ny,self.Nx),dtype = float) + 1
         
-       
         self.V = np.zeros((self.Ny,self.Nx)) + self.pot
         self.V[1:-1,1:-1] = 0
         self.lim = 10e-6
-        
-        
         
         for walls in var['walls']:
             wall = var['walls'][walls]
@@ -430,17 +506,22 @@ class optimal_trajectories:
             plt.show()
         
             
-    def compute_optimal_velocity(self):
+    def mean_field_game(self,m_0,draw, verbose = False):
         
-        m = self.densities
         nx = self.Nx
         ny = self.Ny
         dx = self.dx
         dy = self.dy
-        nt = self.nt_opt
-     
-        def hjb_cole_hopf(t,phi,i):
-        
+        T = self.T
+        dt = self.dt
+        nt = int( T//dt + 1)
+    
+        def phi(t,phi,m,dt):
+            
+            i = int(np.round(t,2)//dt)
+            
+            m_temp = m[:,:,i]*self.evacuator
+            
             phi_temp = np.empty((ny+2,nx+2))
             phi_temp[1:-1,1:-1] = phi.reshape(ny,nx).copy()
             
@@ -453,16 +534,123 @@ class optimal_trajectories:
                               phi_temp[1:-1,:-2] + phi_temp[1:-1,2:] - \
                               4*phi_temp[1:-1,1:-1])/(dx*dy)
             
-            m_temp = np.zeros((ny,nx))
+            phi_temp[1:-1,1:-1] = -0.5*self.sigma**2*lap -\
+                ((self.V+self.g*m_temp)*phi_temp[1:-1,1:-1])/(self.mu*self.sigma**2) 
             
-            if m.shape[0] > 0:
-                m_temp = np.flip(m[i,:].reshape(ny,nx),axis = 0)*self.evacuator 
+            return phi_temp[1:-1,1:-1].reshape(nx*ny)
+
+        def gam(t,gam,m,dt):
             
-            phi_temp[1:-1,1:-1] = -0.5*self.sigma**2*lap - ((self.V+self.g*m_temp)*phi_temp[1:-1,1:-1])/(self.mu*self.sigma**2)
+            i = int(np.round(t,2)//dt)
+            
+            m_temp = m[:,:,i]*self.evacuator
+            
+            gam_temp = np.empty((ny+2,nx+2))
+            gam_temp[1:-1,1:-1] = gam.reshape(ny,nx).copy()
+            
+            gam_temp[0,:] = gam_temp[2,:] 
+            gam_temp[-1,:] = gam_temp[-3,:]
+            gam_temp[:,-1] =  gam_temp[:,-3] 
+            gam_temp[:,0]  =  gam_temp[:,2]  
+            
+            lap = (gam_temp[:-2,1:-1] + gam_temp[2:,1:-1] + gam_temp[1:-1,:-2] +\
+                   gam_temp[1:-1,2:] -4*gam_temp[1:-1,1:-1])/(dx*dy)
+                
+            gam_temp[1:-1,1:-1] = 0.5*self.sigma**2*lap +\
+                ((self.V + self.g*m_temp)*gam_temp[1:-1,1:-1])/(self.mu*self.sigma**2) 
+                        
+            return gam_temp[1:-1,1:-1].reshape(nx*ny)
+        
+        print('Starting MFG!')
+        
+        t_span_phi = (T,0)
+        t_span_gam = (0,T)
+        
+        t_events_phi = np.linspace(T,0,nt)
+        t_events_gam = np.linspace(0,T,nt)
+
+        m_0_total = np.zeros((ny,nx,nt))
+        m_0_total[:,:,0] = m_0
+
+        sol_phi = solve_ivp(phi, t_span_phi, self.phi_0,
+                            method ='RK45',t_eval = t_events_phi, args =(m_0_total,dt))
+        sol_gam = solve_ivp(gam, t_span_gam, m_0.reshape(nx*ny)/sol_phi.y[:,-1],
+                            method ='RK45',t_eval = t_events_gam, args =(m_0_total,dt))
+
+        phi_total = np.flip(sol_phi.y.reshape((ny,nx,nt)),axis = 2)
+        gam_total =  sol_gam.y.reshape((ny,nx,nt))
+        m_total = phi_total*gam_total
+            
+        epoch = 0
+        
+        err = 1
+        
+        while err > 10e-4:
+            
+            sol_phi = solve_ivp(phi, t_span_phi, self.phi_0, 
+                                method ='RK45',t_eval = t_events_phi, args =(m_total,dt))
+            sol_gam = solve_ivp(gam, t_span_gam, m_0.reshape(nx*ny)/sol_phi.y[:,-1], 
+                                method ='RK45',t_eval = t_events_gam, args =(m_total,dt))
+            
+            phi_total = np.flip(sol_phi.y.reshape((ny,nx,nt)),axis = 2)
+            gam_total =  sol_gam.y.reshape((ny,nx,nt))
+            
+            err = np.mean([(phi_total[:,:,i]*gam_total[:,:,i]-\
+                                   m_total[:,:,i])**2 for i in range(nt)])
+            if verbose:
+                print('Epoch {}, error = {:.4f}'.format(epoch,err))
+            
+            m_total = 0.1*phi_total*gam_total + 0.9*m_total
+            
+            epoch+=1
+
+
+        m_total = phi_total*gam_total 
+        
+        if draw: 
+            
+            for t in range(nt):
+                
+                plt.pcolor(self.X_opt,self.Y_opt,m_total[:,:,t]*self.evacuator)
+                plt.title(t)        
+                plt.clim([0,2])
+                plt.colorbar()
+                
+                plt.show()
+            
+        return m_total
+            
+        
+    
+    def compute_optimal_velocity(self):
+        
+        nx = self.Nx
+        ny = self.Ny
+        dx = self.dx
+        dy = self.dy
+        nt = self.nt_opt
+     
+        def hjb(t,phi,i):
+        
+            phi_temp = np.empty((ny+2,nx+2))
+            phi_temp[1:-1,1:-1] = phi.reshape(ny,nx).copy()
+            
+            phi_temp[0,:] = phi_temp[2,:] 
+            phi_temp[-1,:] = phi_temp[-3,:]
+            phi_temp[:,-1] =  phi_temp[:,-3] 
+            phi_temp[:,0]  =  phi_temp[:,2]  
+            
+            lap = (phi_temp[:-2,1:-1] + phi_temp[2:,1:-1] + \
+                              phi_temp[1:-1,:-2] + phi_temp[1:-1,2:] - \
+                              4*phi_temp[1:-1,1:-1])/(dx*dy)
+             
+            
+            phi_temp[1:-1,1:-1] = -0.5*self.sigma**2*lap -\
+                ((self.V)*phi_temp[1:-1,1:-1])/(self.mu*self.sigma**2)
          
             return phi_temp[1:-1,1:-1].reshape(nx*ny)
         
-        def vels_cole_hopf(phi,mu):
+        def vels(phi,mu):
             
             lim = self.lim
             
@@ -488,10 +676,10 @@ class optimal_trajectories:
       
         t_events = np.linspace(self.T,0,self.nt_opt)
 
-        sol = solve_ivp(hjb_cole_hopf, t_span, phi_0, method ='RK45',t_eval = t_events,args = (0,))
+        sol = solve_ivp(hjb, t_span, phi_0, method ='RK45',t_eval = t_events,args = (0,))
         
         for i in np.arange(nt-1,0,-1):
-            vx,vy = vels_cole_hopf(sol.y[:,nt - i ],self.mu)
+            vx,vy = vels(sol.y[:,nt - i ],self.mu)
             self.vx_opt[i-1] = vx
             self.vy_opt[i-1] = vy
 
